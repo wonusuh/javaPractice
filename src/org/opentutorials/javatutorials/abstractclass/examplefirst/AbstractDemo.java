c53685130522ccc6f4506af6a8e463e8eb712541
wonusuh <wonwooseo23@gmail.com>
2020-06-02 12:48:20 +0900

    modified some

--- src/org/opentutorials/javatutorials/abstractclass/examplefirst/AbstractDemo.java
+++ src/org/opentutorials/javatutorials/abstractclass/examplefirst/AbstractDemo.java
@@ -11,8 +11,18 @@ package org.opentutorials.javatutorials.abstractclass.examplefirst;
 abstract class A {
 	public abstract int b();
 
+//	public abstract int c() {
+//		System.out.println("Hello");
+//	}
+
 	public void d() {
 		System.out.println("world");
+	}
+}
+
+class B extends A {
+	public int b() {
+		return 1;
 	}
 }
 
@@ -23,7 +33,7 @@ public class AbstractDemo {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		A obj = new A();
+		B obj = new B();
 	}
 
 }
