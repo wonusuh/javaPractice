838d7d6c7b93830414289be7f18e7fdead0f4fbe
user <user@192.168.219.102>
2023-05-26 00:40:20 +0900

    connecting

---
+++ src/org/opentutorials/javatutorialssecond/Helloworld.java
@@ -0,0 +1,10 @@
+package org.opentutorials.javatutorialssecond;
+
+public class Helloworld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("world Hello");
+	}
+
+}
