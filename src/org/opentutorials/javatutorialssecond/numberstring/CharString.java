55e9e8fa956e3e69b05d5796ab1109d4dabdbfa2
user <user@192.168.219.102>
2023-05-26 18:39:10 +0900

    modified

---
+++ src/org/opentutorials/javatutorialssecond/numberstring/CharString.java
@@ -0,0 +1,12 @@
+package org.opentutorials.javatutorialssecond.numberstring;
+
+public class CharString {
+
+	public static void main(String[] args) {
+		System.out.println(1 + 1);
+		System.out.println("1" + "1");
+		System.out.println("egoing said \"Welcom programming world\"");
+		System.out.println("egoing said \n\"Welcom programming world\"");
+	}
+
+}
