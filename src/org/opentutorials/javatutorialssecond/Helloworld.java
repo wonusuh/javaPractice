85a9429c9b0668ae7c062c4651842c77c6098336
user <user@192.168.219.102>
2023-05-26 17:08:35 +0900

    some modified

--- src/org/opentutorials/javatutorialssecond/Helloworld.java
+++ src/org/opentutorials/javatutorialssecond/Helloworld.java
@@ -4,7 +4,7 @@ public class Helloworld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("world Hello");
+		System.out.println("world Hello modified");
 	}
 
 }
