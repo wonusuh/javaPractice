2d29289f7e1e4494fae50afe65baae5b1934c1e9
wonusuh <wonwooseo23@gmail.com>
2023-05-29 18:58:58 +0900

    230529

---
+++ src/org/opentutorials/javatutorialssecond/condition/Condition2Demo.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package org.opentutorials.javatutorialssecond.condition;
+
+/**
+ * @author user
+ *
+ */
+public class Condition2Demo {
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		if (false) {
+			System.out.println(1);
+			System.out.println(2);
+			System.out.println(3);
+			System.out.println(4);
+		}
+		System.out.println(5);
+	}
+
+}
---
+++ src/org/opentutorials/javatutorialssecond/condition/SwitchDemo2.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package org.opentutorials.javatutorialssecond.condition;
+
+/**
+ * @author user
+ *
+ */
+public class SwitchDemo2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int val = 4;
+		if (val == 1) {
+			System.out.println("one");
+		} else if (val == 2) {
+			System.out.println("two");
+		} else if (val == 3) {
+			System.out.println("three");
+		} else {
+			System.out.println("error");
+		}
+
+	}
+
+}
