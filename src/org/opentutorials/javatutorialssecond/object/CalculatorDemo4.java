7942bc4168e5e5dfd0beddea3c00387eb9bb24dc
wonusuh <wonwooseo23@gmail.com>
2023-06-02 16:30:03 +0900

    230602

---
+++ src/org/opentutorials/javatutorialssecond/object/CalculatorDemo4.java
@@ -0,0 +1,30 @@
+package org.opentutorials.javatutorialssecond.object;
+
+class Calculator {
+	int left, right;
+
+	public void setOperands(int left, int right) {
+		this.left = left;
+		this.right = right;
+	}
+
+	public void sum() {
+		System.out.println(this.left + this.right);
+	}
+
+	public void avg() {
+		System.out.println((this.left + this.right) / 2);
+	}
+}
+
+public class CalculatorDemo4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Calculator c1 = new Calculator();
+		c1.setOperands(10, 20);
+		c1.sum();
+		c1.avg();
+	}
+
+}
