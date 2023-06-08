d8f7fdd70a51c534c1f49f5db45043d67642bff0
wonusuh <wonwooseo23@gmail.com>
2023-06-09 07:12:37 +0900

    230609

---
+++ src/org/opentutorials/javatutorialssecond/library/LibraryDemo1.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package org.opentutorials.javatutorialssecond.library;
+
+import java.lang.*;
+
+/**
+ * @author user
+ *
+ */
+public class LibraryDemo1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(1);
+	}
+
+}
---
+++ src/org/opentutorials/javatutorialssecond/packages/example3/D.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package org.opentutorials.javatutorialssecond.packages.example3;
+
+import org.opentutorials.javatutorialssecond.packages.example1.*;
+import org.opentutorials.javatutorialssecond.packages.example2.*;
+
+/**
+ * @author user
+ *
+ */
+public class D {
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		org.opentutorials.javatutorialssecond.packages.example2.B b = new org.opentutorials.javatutorialssecond.packages.example2.B();
+	}
+}
