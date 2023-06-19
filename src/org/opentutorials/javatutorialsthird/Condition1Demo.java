f0532bf1d8136ade3b958d2189d4e500e00e1a96
wonusuh <wonwooseo23@gmail.com>
2023-06-19 19:28:32 +0900

    230619

---
+++ src/org/opentutorials/javatutorialsthird/Condition1Demo.java
@@ -0,0 +1,9 @@
+package org.opentutorials.javatutorialsthird;
+
+public class Condition1Demo {
+	public static void main(String[] args) {
+		if (true) {
+			System.out.println("result : true");
+		}
+	}
+}
