49420102486a4bcb82a74a3a13cc45ebe98b34d6
wonusuh <wonwooseo23@gmail.com>
2023-06-21 06:34:33 +0900

    230621

---
+++ src/org/opentutorials/javatutorialsthird/scope/ScopeDemo5.java
@@ -0,0 +1,10 @@
+package org.opentutorials.javatutorialsthird.scope;
+
+public class ScopeDemo5 {
+	public static void main(String[] args) {
+		for (int i = 0; i < 5; i++) {
+			System.out.println(i);
+		}
+		// System.out.println(i);
+	}
+}
