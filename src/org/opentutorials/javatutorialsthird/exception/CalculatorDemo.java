e4c9177d26c1b280c823e5d20346bb59b066a3f5
wonusuh <wonwooseo23@gmail.com>
2023-06-26 09:05:25 +0900

    230626

--- src/org/opentutorials/javatutorialsthird/exception/CalculatorDemo.java
+++ src/org/opentutorials/javatutorialsthird/exception/CalculatorDemo.java
@@ -9,20 +9,29 @@ class Calculator {
 	}
 
 	public void divide() {
+		if (right == 0) {
+			throw new ArithmeticException("0으로 나눌 수 없습니다.");
+		}
 		try {
 			System.out.print("계산결과는 ");
 			System.out.print(this.left / this.right);
 			System.out.println(" 입니다.");
 		} catch (Exception e) {
 			System.out.println("\n\ne.getMessage()\n" + e.getMessage());
+			System.out.println("\n\ne.toString()\n" + e.toString());
+			System.out.println("\n\ne.printStackTrace()");
+			e.printStackTrace();
 		}
-		System.out.println("divide() ended");
 	}
 }
 
 public class CalculatorDemo {
 	public static void main(String[] args) {
 		Calculator c1 = new Calculator(10, 0);
-		c1.divide();
+		try {
+			c1.divide();
+		} catch (ArithmeticException e) {
+			System.out.println(e.getMessage());
+		}
 	}
 }
