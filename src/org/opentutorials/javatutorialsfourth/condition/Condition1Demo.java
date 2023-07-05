1455105ffeb6983635885b41e904fc3d043a08b9
wonusuh <wonwooseo23@gmail.com>
2023-07-05 16:55:16 +0900

    230705

---
+++ src/org/opentutorials/javatutorialsfourth/condition/Condition1Demo.java
@@ -0,0 +1,9 @@
+package org.opentutorials.javatutorialsfourth.condition;
+
+public class Condition1Demo {
+	public static void main(String[] args) {
+		if (true) {
+			System.out.println("result : ture");
+		}
+	}
+}
---
+++ src/org/opentutorials/javatutorialsfourth/condition/LoginDemo.java
@@ -0,0 +1,12 @@
+package org.opentutorials.javatutorialsfourth.condition;
+
+public class LoginDemo {
+	public static void main(String[] args) {
+		String id = args[0];
+		if (id.equals("egoing")) {
+			System.out.println("right");
+		} else {
+			System.out.println("wrong");
+		}
+	}
+}
