dc1becf64c27e2564f398fb05f0cec44c24b9476
wonusuh <wonwooseo23@gmail.com>
2023-07-08 20:00:26 +0900

    object and instance

---
+++ src/org/opentutorials/javatutorialsfourth/object/CalculatorDemo.java
@@ -0,0 +1,8 @@
+package org.opentutorials.javatutorialsfourth.object;
+
+public class CalculatorDemo {
+	public static void main(String[] args) {
+		System.out.println(10 + 20);
+		System.out.println(20 + 40);
+	}
+}
---
+++ src/org/opentutorials/javatutorialsfourth/object/CalculatorDemo2.java
@@ -0,0 +1,12 @@
+package org.opentutorials.javatutorialsfourth.object;
+
+public class CalculatorDemo2 {
+	public static void sum(int init, int right) {
+		System.out.println(init + right);
+	}
+
+	public static void main(String[] args) {
+		sum(10, 20);
+		sum(20, 40);
+	}
+}
