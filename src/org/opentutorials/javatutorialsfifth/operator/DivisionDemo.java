5bd6fe3cb95a984f403a2be84f2ef52a4ba5f28b
wonusuh <wonwooseo23@gmail.com>
2023-07-20 23:52:45 +0900

    operators

---
+++ src/org/opentutorials/javatutorialsfifth/operator/DivisionDemo.java
@@ -0,0 +1,13 @@
+package org.opentutorials.javatutorialsfifth.operator;
+
+public class DivisionDemo {
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 3;
+		float c = (float) 10.0;
+		float d = (float) 3.0;
+		System.out.println(a / b);
+		System.out.println(c / d);
+		System.out.println(a / d);
+	}
+}
