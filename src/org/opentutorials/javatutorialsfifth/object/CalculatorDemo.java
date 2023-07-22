d27071138287bfe734d9660eb98d91ef5f31da80
wonusuh <wonwooseo23@gmail.com>
2023-07-22 21:39:04 +0900

    object oriented programming

---
+++ src/org/opentutorials/javatutorialsfifth/object/CalculatorDemo.java
@@ -0,0 +1,8 @@
+package org.opentutorials.javatutorialsfifth.object;
+
+public class CalculatorDemo {
+	public static void main(String[] args) {
+		System.out.println(10 + 20);
+		System.out.println(20 + 40);
+	}
+}
