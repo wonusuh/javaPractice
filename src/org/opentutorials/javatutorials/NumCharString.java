5fd339eca3ade37edc81969df7a7aca73cf9d626
wonusuh <wonwooseo23@gmail.com>
2023-07-23 23:26:26 +0900

    constructor

--- src/org/opentutorials/javatutorials/NumCharString.java
+++ src/org/opentutorials/javatutorials/NumCharString.java
@@ -14,27 +14,27 @@ public class NumCharString {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-//		숫자
+
+//		�レ��
 		System.out.println(1 + 2);
 		System.out.println(1.2 + 1.3);
 		System.out.println(2 * 5);
 		System.out.println(6 / 2);
 
-//		문자와 문자열
-		System.out.println('생');
-		System.out.println("생활코딩");
-//		System.out.println('생활코딩');
-		System.out.println("생");
+//		臾몄���� 臾몄����
+		System.out.println("");
+		System.out.println("����肄���");
+//		System.out.println('����肄���');
+		System.out.println("��");
 
-//		이스케이프(문자로 해석하도록 강제 \")
+//		�댁�ㅼ��댄��(臾몄��濡� �댁������濡� 媛��� \")
 		System.out.println("egoing said \"Welcome programming world\"");
 
-//		여러 줄의 표시(\n)
+//		�щ�� 以��� ����(\n)
 		System.out.println("HTML\nCSS\nJavaScript\n");
 
-//		문자의 연산
-		System.out.println("생활" + "코딩");
+//		臾몄���� �곗��
+		System.out.println("����" + "肄���");
 
 	}
 
---
+++ src/org/opentutorials/javatutorialsfifth/scope/ScopeDemo.java
@@ -0,0 +1,18 @@
+package org.opentutorials.javatutorialsfifth.scope;
+
+class C {
+	int v = 10;
+
+	void m() {
+		int v = 20;
+		System.out.println(v);
+		System.out.println(this.v);
+	}
+}
+
+public class ScopeDemo {
+	public static void main(String[] args) {
+		C c1 = new C();
+		c1.m();
+	}
+}
