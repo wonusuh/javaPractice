9ef7a224efea24dca280fa62d9db1b305a97ef5f
wonusuh <wonwooseo23@gmail.com>
2023-08-04 20:54:41 +0900

    반복과정에서 조건 판단하기 1

--- src/practiceSource/chap01/SumVerbose1.java
+++ src/practiceSource/chap01/SumVerbose1.java
@@ -4,27 +4,28 @@ package practicesource.chap01;
 import java.util.Scanner;
 
 class SumVerbose1 {
-    public static void main(String[] args) {
-        Scanner stdIn = new Scanner(System.in);
-        int n;
+	public static void main(String[] args) {
+		Scanner stdIn = new Scanner(System.in);
+		int n;
 
-        System.out.println("1부터 n까지의 합을 구합니다.");
+		System.out.println("1부터 n까지의 합을 구합니다.");
 
-        do {
-            System.out.print("n값: ");
-            n = stdIn.nextInt();
-        } while (n <= 0);
+		do {
+			System.out.print("n값 : ");
+			n = stdIn.nextInt();
+		} while (n <= 0);
 
-        int sum = 0;        // 합
+		int sum = 0; // 합
 
-        for (int i = 1; i <= n; i++) {
-            if (i < n)        // 중간 과정
-                System.out.print(i + " + ");
-            else              // 마지막 과정
-                System.out.print(i + " = ");
-            sum += i;         // sum에 i를 더함
-        }
+		for (int i = 1; i <= n; i++) {
+			if (i < n) // 중간 과정
+				System.out.print(i + " + ");
+			else // 마지막 과정
+				System.out.print(i + " = ");
+			sum += i; // sum에 i를 더함
+		}
 
-        System.out.println(sum);
-    }
+		System.out.println(sum);
+		stdIn.close();
+	}
 }
