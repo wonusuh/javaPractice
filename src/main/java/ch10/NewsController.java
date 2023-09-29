9579d29ac533c8af786c598a461511a2a5a21d50
wonusuh <wonwooseo23@gmail.com>
2023-09-29 22:06:26 +0900

    modifing method service()

--- src/main/java/ch10/NewsController.java
+++ src/main/java/ch10/NewsController.java
@@ -1,6 +1,8 @@
 package ch10;
 
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import javax.servlet.Servlet;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletContext;
@@ -38,5 +40,33 @@ public class NewsController extends HttpServlet {
 	 */
 	protected void service(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		String action = request.getParameter("action");
+
+		dao = new NewsDAO();
+
+		// 자바 리플렉션을 사용해 if(switch) 없이 요청에 따라 구현 메서드가 실행되도록 구성
+		Method m;
+		String view = null;
+
+		// action 파라미터 없이 접근한 경우
+		if (action == null) {
+			action = "listNews";
+		}
+
+		try {
+			// 현재 클래스에서 action 이름과 HttpServletRequest를 파라미터로 갖는 메서드 찾음
+			m = this.getClass().getMethod(action, HttpServletRequest.class);
+
+			// 메서드 실행 후 리턴값을 받아옴
+			view = (String) m.invoke(this, request);
+		} catch (NoSuchMethodException e) {
+			e.printStackTrace();
+			ctx.log("요청 action 없음!!");
+			request.setAttribute("error", "action의 파라미터가 잘못되었습니다!!");
+			view = START_PAGE;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
