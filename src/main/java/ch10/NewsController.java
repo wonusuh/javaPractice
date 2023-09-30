43fdd7450d35d77e06ad5b1b840b5a9a88cc9ace
wonusuh <wonwooseo23@gmail.com>
2023-09-30 22:11:55 +0900

    POST request handling (refresh issue)

--- src/main/java/ch10/NewsController.java
+++ src/main/java/ch10/NewsController.java
@@ -1,9 +1,8 @@
 package ch10;
 
 import java.io.IOException;
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import javax.servlet.Servlet;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
@@ -13,36 +12,26 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class NewsController
- */
 @WebServlet(description = "news project controller", urlPatterns = { "/news.nhn" })
-@MultipartConfig(maxFileSize = 1024 * 1024 * 2, location = "D:\\suh\\programs\\Temporary\\img")
+@MultipartConfig(maxFileSize = 1024 * 1024 * 2, location = "c:/Temp/img")
 public class NewsController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private NewsDAO dao;
 	private ServletContext ctx;
+
 	// 웹 리소스 기본 경로 지정
 	private final String START_PAGE = "ch10/newsList.jsp";
 
-	/**
-	 * @see Servlet#init(ServletConfig)
-	 */
 	public void init(ServletConfig config) throws ServletException {
 		super.init(config);
 		dao = new NewsDAO();
 		ctx = getServletContext();
 	}
 
-	/**
-	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse
-	 *      response)
-	 */
 	protected void service(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		request.setCharacterEncoding("utf-8");
 		String action = request.getParameter("action");
-
 		dao = new NewsDAO();
 
 		// 자바 리플렉션을 사용해 if(switch) 없이 요청에 따라 구현 메서드가 실행되도록 구성
@@ -53,7 +42,6 @@ public class NewsController extends HttpServlet {
 		if (action == null) {
 			action = "listNews";
 		}
-
 		try {
 			// 현재 클래스에서 action 이름과 HttpServletRequest를 파라미터로 갖는 메서드 찾음
 			m = this.getClass().getMethod(action, HttpServletRequest.class);
@@ -67,6 +55,15 @@ public class NewsController extends HttpServlet {
 			view = START_PAGE;
 		} catch (Exception e) {
 			e.printStackTrace();
+		}
+
+		// POST 요청에서는 리디렉션 방법으로 이동하도록 분기
+		if (view.startsWith("redirect:/")) {
+			String rview = view.substring("redirect:/".length());
+			response.sendRedirect(rview);
+		} else {
+			RequestDispatcher dispatcher = request.getRequestDispatcher(view);
+			dispatcher.forward(request, response);
 		}
 	}
 }
