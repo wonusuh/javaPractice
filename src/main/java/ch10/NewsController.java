b668a05be9d8ed2cb9c12cd19a774a3d93106d17
wonusuh <wonwooseo23@gmail.com>
2023-10-03 18:53:00 +0900

    added servlet

---
+++ src/main/java/ch10/NewsController.java
@@ -0,0 +1,44 @@
+package ch10;
+
+import java.io.IOException;
+import javax.servlet.Servlet;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class NewsController
+ */
+@WebServlet(description = "my news controller", urlPatterns = { "/news.nhn" })
+@MultipartConfig(maxFileSize = 1024 * 1024 * 2, location = "C:\\Temp\\img")
+public class NewsController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	private NewsDAO dao;
+	private ServletContext ctx;
+
+	// 웹 리소스 기본 경로 지정
+	private final String START_PAGE = "ch10/newsList.jsp";
+
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	public void init(ServletConfig config) throws ServletException {
+		super.init(config);
+		dao = new NewsDAO();
+		ctx = getServletContext();
+	}
+
+	/**
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void service(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+	}
+}
