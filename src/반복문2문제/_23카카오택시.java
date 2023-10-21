1f49aebf8305f85f0bf75450729d2aa43e125d77
wonusuh <wonwooseo23@gmail.com>
2023-10-21 19:50:22 +0900

    kakao taxi review

--- src/반복문2문제/_23카카오택시.java
+++ src/반복문2문제/_23카카오택시.java
@@ -12,24 +12,22 @@ public class _23카카오택시 {
 //				1) 속도설정 : 1~3까지만 가능
 //				2) 방향 설정 : 동(1)서(2)남(3)북(4)
 //				3) 이동하기 : 설정된 방향으로 설정된 속도만큼 이동
+//				4) 종료
 //			4. 거리 1칸당 50원씩 추가되어 도착시 요금도 출력한다
 //			5. 도착하면 다시 -10 ~ 10 사이의 랜덤 목적지가 설정된다
+		// 도착하면 전체수입에 요금을 추가한다
 		// 속도나 방향이 0이면 이동 불가;
 		Scanner sc = new Scanner(System.in);
 		Random rn = new Random();
-
 		int dir = 0; // 방향
 		int x = 0; // 출발지점 가로
 		int y = 0; // 출발지점 세로
 		int dx = 0; // 도착지점 가로
 		int dy = 0; // 도착지점 세로
 		int speed = 0; // 속도
-
 		int crnCost = 0; // 현재 요금
 		int earnings = 0; // 전체수익
-
 		boolean run = true;
-
 		dx = rn.nextInt(10 - -10 + 1) + -10;
 		dy = rn.nextInt(10 - -10 + 1) + -10;
 		while (run) {
@@ -38,7 +36,6 @@ public class _23카카오택시 {
 			System.out.printf("[방향] : %d [속도] : %d\n", dir, speed);
 			System.out.printf("[현재요금] : %d [전체수익] : %d\n", crnCost, earnings);
 			System.out.printf("[1.속도] [2.방향] [3.이동] [0.종료]\n");
-
 			int menu = sc.nextInt();
 			if (menu == 1) {
 				while (true) {
