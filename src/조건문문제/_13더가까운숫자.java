125ff95080fada61905012bfee847f033709a436
wonusuh <wonwooseo23@gmail.com>
2023-10-22 17:11:03 +0900

    reviewed more close num

--- src/조건문문제/_13더가까운숫자.java
+++ src/조건문문제/_13더가까운숫자.java
@@ -7,16 +7,12 @@ public class _13더가까운숫자 {
 //		랜덤으로 1~150 사이의 값을 변수 a,b,c 에 넣고 값을 출력하시오
 //		그리고 a 와 c 중에 어떤숫자가 b 와 더 가까운지 출력하시오
 		Random rn = new Random();
-		System.out.println("1~150 사이의 정수 a, b, c 를 비교합니다");
 		int a = rn.nextInt(150 - 1 + 1) + 1;
-		System.out.println("a : " + a);
 		int b = rn.nextInt(150 - 1 + 1) + 1;
-		System.out.println("b : " + b);
 		int c = rn.nextInt(150 - 1 + 1) + 1;
-		System.out.println("c : " + c);
+		System.out.printf("a == %d, b == %d, c == %d\n", a, b, c);
 		int ab = 0;
 		int cb = 0;
-
 		if (a < b) {
 			ab = b - a;
 		}
@@ -29,7 +25,6 @@ public class _13더가까운숫자 {
 		if (c > b) {
 			cb = c - b;
 		}
-
 		if (ab < cb) {
 			System.out.println("a가 b에 더 가깝다");
 		}
