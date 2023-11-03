a7f769d31dcbb67c2bd12ad6c98876e7606c415a
wonusuh <wonwooseo23@gmail.com>
2023-11-03 18:49:00 +0900

    question 10 left

--- src/_level3test/문제10.java
+++ src/_level3test/문제10.java
@@ -1,6 +1,57 @@
 package _level3test;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class 문제10 {
 	public static void main(String[] args) {
+//		 * # 미니마블
+//		 * 1. 플레이어는 p1과 p2 2명이다.
+//		 * 2. 번갈아가며 1~3 사이의 숫자를 입력해 이동한다.
+//		 * 3. 이동하다가 다음 플레이어와 같은 위치에 놓이게 되면,
+//		 *    상대 플레이어는 잡히게 되어 원점으로 되돌아간다.
+//		 * 4. 먼저 3바퀴를 돌면 이긴다.
+
+//		 *  [p2]1~3 입력 : 1
+//		 *  1 2 3 4 5 6 7 8 
+//		 *  0 1 0 0 0 0 0 0 
+//		 *  0 0 0 2 0 0 0 0 
+
+//		 *  [p1]1~3 입력 : 3
+//		 *  [p1]이 p2를 잡았다!
+//		 *  1 2 3 4 5 6 7 8 
+//		 *  0 0 0 0 1 0 0 0 
+//		 *  2 0 0 0 0 0 0 0 
+		int[] game = { 1, 2, 3, 4, 5, 6, 7, 8 };
+		int[] p1 = { 0, 0, 0, 0, 0, 0, 0, 0 };
+		int[] p2 = { 0, 0, 0, 0, 0, 0, 0, 0 };
+
+		int turn = 1;
+
+		int idx1 = 0;
+		int idx2 = 0;
+		p1[idx1] = 1;
+		p2[idx2] = 2;
+
+		Scanner sc = new Scanner(System.in);
+
+		int p1cnt = 0;
+		int p2cnt = 0;
+
+		while (true) {
+			System.out.printf("[p1] %d바퀴 [p2] %d바퀴\n", p1cnt, p1cnt);
+			System.out.println("game" + Arrays.toString(game));
+			System.out.println(" p1 " + Arrays.toString(p1));
+			System.out.println(" p2 " + Arrays.toString(p2));
+			System.out.printf("[%d턴] 입력(0~3) >> ", turn);
+
+			int input = sc.nextInt();
+
+			if (p1cnt == 3 || p2cnt == 3) {
+				System.out.printf("[%d] 승리!", turn);
+				break;
+			}
+		}
+		sc.close();
 	}
 }
--- src/_level3test/문제9.java
+++ src/_level3test/문제9.java
@@ -16,14 +16,18 @@ public class 문제9 {
 //		[O][ ][ ]
 //		[p1]승리
 		Scanner sc = new Scanner(System.in);
+
 		int size = 9;
 		int[] game = new int[size];
 		int turn = 1; // player1 player2 // 1 2
 		int win = 0; // 플레이어1 승리 플레이어 2승리
 		int cnt = 0;
+		boolean run = true;
 
-		while (true) {
+		while (run) {
+			System.out.println("=====틱택토=====");
 
+			// 틱택토 게임시트 출력
 			for (int i = 0; i < size; i += 1) {
 
 				if (game[i] == 1) {
@@ -38,16 +42,73 @@ public class 문제9 {
 					System.out.println();
 				}
 			}
-			
 
-			if (turn == 1) {
-			} else if (turn == 2) {
-			}
-
+			// 비기는 경우
 			if (cnt == 9) {
-				System.out.println("비겼다");
+				System.out.println("비겼다.");
 				break;
 			}
+			System.out.printf("[%d]인덱스 입력 : ", turn);
+
+			int idx = sc.nextInt();
+
+			System.out.println();
+
+			// 인덱스 범위 예외
+			if (idx < 0 || idx > size - 1) {
+				System.out.println("인덱스를 확인하세요.");
+				continue;
+			}
+
+			// 이미 표기된 인덱스 예외
+			if (game[idx] != 0) {
+				System.out.println("그 인덱스에 다른 플레이어가 이미 표기했습니다.");
+				continue;
+			}
+			game[idx] = turn;
+
+			// 가로 승리조건
+			for (int i = 0; i < size; i += 3) {
+				if (game[i] == turn && game[i + 1] == turn && game[i + 2] == turn) {
+					win = turn;
+					System.out.print("가로 ");
+				}
+			}
+
+			// 세로 승리조건
+			for (int i = 0; i < size - 6; i += 1) {
+				if (game[i] == turn && game[i + 3] == turn && game[i + 6] == turn) {
+					win = turn;
+					System.out.print("세로 ");
+				}
+			}
+
+			// 대각(\) 승리조건
+			if (game[0] == turn && game[4] == turn && game[8] == turn) {
+				win = turn;
+				System.out.print("대각(\\) ");
+			}
+
+			// 대각(/) 승리조건
+			if (game[2] == turn && game[4] == turn && game[6] == turn) {
+				win = turn;
+				System.out.print("대각(/) ");
+			}
+
+			// 승리 출력
+			if (win != 0) {
+				System.out.printf("[p%d] 승리!", turn);
+				run = false;
+				break;
+			}
+
+			// 턴 전환하기
+			if (turn == 1) {
+				turn = 2;
+			} else if (turn == 2) {
+				turn = 1;
+			}
+			cnt += 1;
 		}
 		sc.close();
 	}
