368e6c53ad366e8d286c586ef195b494efaf42b9
wonusuh <wonwooseo23@gmail.com>
2023-11-18 21:54:33 +0900

    폭파 수정중

--- src/배열심화_문제3/_28폭파.java
+++ src/배열심화_문제3/_28폭파.java
@@ -1,5 +1,6 @@
 package 배열심화_문제3;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class _28폭파 {
@@ -39,5 +40,65 @@ public class _28폭파 {
 
 		bombs[4][0] = 2;
 		bombs[4][1] = 1;
+
+		while (true) {
+			System.out.println("bombs[][] == " + Arrays.deepToString(bombs));
+
+			// 출력
+			for (int i = 0; i < size; i += 1) {
+
+				for (int j = 0; j < size; j += 1) {
+
+					if (map[i][j] == bomb) {
+						System.out.print(String.format("%-3s", "◐"));
+					} else if (map[i][j] == wall) {
+						System.out.print(String.format("%-3s", "■"));
+					} else if (map[i][j] == road) {
+						System.out.print(String.format("%-3s", "+"));
+					}
+				}
+				System.out.println();
+			}
+
+			System.out.print("폭파(6)");
+			int input = sc.nextInt();
+
+			if (input == 6) {
+				// 폭파
+
+				int y = bombs[0][0];
+				int x = bombs[0][1];
+
+				bombs[0] = null;
+
+				for (int i = 1; i < bombs.length; i += 1) {
+					
+					
+
+					if (y - 1 < 0) {
+						y = 0;
+					}
+
+					if (x - 1 < 0) {
+						x = 0;
+					}
+
+					if (y + 1 > size - 1) {
+						y = size - 1;
+					}
+
+					if (x + 1 > size - 1) {
+						x = size - 1;
+					}
+
+					if (bombs[i][0] == y && bombs[i][1] == x) {
+						System.out.print(bombs[i]);
+					}
+				}
+			} else if (input == -1) {
+				break;
+			}
+		}
+		sc.close();
 	}
 }
