f5650b4ba995214a3f67cd0db408b31d6c792d6d
wonusuh <wonwooseo23@gmail.com>
2023-12-02 13:14:31 +0900

    class array Tictactoe complited

--- src/클래스배열_문제/_07클배틱택토.java
+++ src/클래스배열_문제/_07클배틱택토.java
@@ -13,7 +13,8 @@ class Tic {// 가로 한 줄을 한 개의 객체로 본다.
 class Tictactoe {// 게임 구현
 	Tic[] tics;// 3개
 	Scanner sc = new Scanner(System.in);
-	int turn = 1, win;
+	int turn = 1, cnt;
+	String condition;
 
 	void init() {
 		tics = new Tic[3];
@@ -45,27 +46,102 @@ class Tictactoe {// 게임 구현
 		return check;
 	}
 
+	boolean isWritten(int y, int x) {
+		boolean check = false;
+		if (tics[y].line[x] != 0) {
+			check = true;
+		}
+		return check;
+	}
+
 	void writeOX() {
+		int y, x;
 		while (true) {
 			System.out.printf("[p%d]의 턴\n", turn);
 			System.out.print("y좌표를 입력하세요. >> ");
-			int y = sc.nextInt();
+			y = sc.nextInt();
 			System.out.print("x좌표를 입력하세요. >> ");
-			int x = sc.nextInt();
+			x = sc.nextInt();
 			if (isOutOf(y, x)) {
 				System.err.println("인덱스를 확인하세요.");
 				continue;
 			}
+			if (isWritten(y, x)) {
+				System.err.println("다른 플레이어가 이미 표기한 위치입니다.");
+				continue;
+			}
 			break;
 		}
+		tics[y].line[x] = turn == 1 ? 1 : 2;
+		cnt += 1;
+	}
+
+	void changeTurn() {
+		turn = turn == 1 ? 2 : 1;
+	}
+
+	boolean isDraw() {
+		boolean check = false;
+		if (cnt >= 9) {
+			System.out.println("비겼습니다.");
+			check = true;
+		}
+		return check;
+	}
+
+	boolean isVertical() {
+		boolean check = false;
+		for (Tic t : tics) {
+			if (t.line[0] != 0 && t.line[0] == t.line[1] && t.line[1] == t.line[2]) {
+				check = true;
+				condition = "가로";
+			}
+		}
+		return check;
+	}
+
+	boolean isHorizon() {
+		boolean check = false;
+		for (int i = 0; i < tics[0].line.length; i += 1) {
+			if (tics[0].line[i] != 0 && tics[0].line[i] == tics[1].line[i] && tics[1].line[i] == tics[2].line[i]) {
+				check = true;
+				condition = "세로";
+			}
+		}
+		return check;
+	}
+
+	boolean isBackSlash() {
+		boolean check = false;
+		if (tics[0].line[0] != 0 && tics[0].line[0] == tics[1].line[1] && tics[1].line[1] == tics[2].line[2]) {
+			check = true;
+			condition = "\\";
+		}
+		return check;
+	}
+
+	boolean isSlash() {
+		boolean check = false;
+		if (tics[0].line[2] != 0 && tics[0].line[2] == tics[1].line[1] && tics[1].line[1] == tics[2].line[0]) {
+			check = true;
+			condition = "/";
+		}
+		return check;
 	}
 
 	void run() {
 		init();
-		showGame();
 		while (true) {
+			showGame();
+			if (isHorizon() || isVertical() || isBackSlash() || isSlash()) {
+				turn = turn == 1 ? 2 : 1;
+				System.out.printf("[p%d] %s 승리!", turn, condition);
+				break;
+			}
+			if (isDraw())
+				break;
 			writeOX();
-			break;
+			changeTurn();
 		}
 	}
 }
