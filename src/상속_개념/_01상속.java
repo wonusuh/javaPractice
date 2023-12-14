234cf80ba051c5d24390dc674e0a8cff91bdd7d7
wonusuh <wonwooseo23@gmail.com>
2023-12-14 15:30:04 +0900

    updated : _01상속

--- src/상속_개념/_01상속.java
+++ src/상속_개념/_01상속.java
@@ -2,6 +2,8 @@ package 상속_개념;
 
 public class _01상속 {
 	public static void main(String[] args) {
-		System.out.println("상속이란");
+		System.out.println("상속이란1");
+		System.out.println("test2");
+		System.out.println("test3");
 	}
 }
