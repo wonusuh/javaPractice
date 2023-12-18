c597a5402d189bfe52afc2d214c5ff79b6ecb546
wonusuh <wonwooseo23@gmail.com>
2023-12-18 11:40:40 +0900

    first commit

---
+++ src/SchoolVer4_서원우/Student.java
@@ -0,0 +1,49 @@
+package SchoolVer4_서원우;
+
+public class Student {
+
+	private int stuNo;
+	private String stuId, stuName;
+
+	/**
+	 * @return the stuNo
+	 */
+	public int getStuNo() {
+		return stuNo;
+	}
+
+	/**
+	 * @param stuNo the stuNo to set
+	 */
+	public void setStuNo(int stuNo) {
+		this.stuNo = stuNo;
+	}
+
+	/**
+	 * @return the stuId
+	 */
+	public String getStuId() {
+		return stuId;
+	}
+
+	/**
+	 * @param stuId the stuId to set
+	 */
+	public void setStuId(String stuId) {
+		this.stuId = stuId;
+	}
+
+	/**
+	 * @return the stuName
+	 */
+	public String getStuName() {
+		return stuName;
+	}
+
+	/**
+	 * @param stuName the stuName to set
+	 */
+	public void setStuName(String stuName) {
+		this.stuName = stuName;
+	}
+}
